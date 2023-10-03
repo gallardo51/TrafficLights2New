5ae6ed521d4715cd0ed9b2e17a6dea11b675ebb3
Alex Sobolev <31108@mail.ru>
2023-10-03 17:12:41 +0300

    Initial Commit

---
+++ TrafficLights2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLights2
+//
+//  Created by Александр Соболев on 03.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
