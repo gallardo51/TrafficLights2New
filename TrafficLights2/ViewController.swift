620ddf9675fc84fe694d7a04f9ea18a14775932e
Alex Sobolev <31108@mail.ru>
2023-10-03 19:11:54 +0300

    Final setup

--- TrafficLights2/ViewController.swift
+++ TrafficLights2/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var redLightLabelView: UIView!
     @IBOutlet weak var yellowLightLabelView: UIView!
     @IBOutlet weak var greenLightLabelView: UIView!
@@ -27,9 +28,28 @@ class ViewController: UIViewController {
         
         startButtonLabelButton.layer.cornerRadius = 10
     }
-
-    @IBAction func startButtonPressed() {
-    }
     
-}
+    @IBAction func startButtonPressed() {
+        
+        if isLightOn(redLightLabelView) {
+            redLightLabelView.alpha = 0.3
+            yellowLightLabelView.alpha = 1
+        } else if isLightOn(yellowLightLabelView) {
+            yellowLightLabelView.alpha = 0.3
+            greenLightLabelView.alpha = 1
+        } else {
+            greenLightLabelView.alpha = 0.3
+            redLightLabelView.alpha = 1
+        }
+        
+        if startButtonLabelButton.currentTitle == "START" {
+            startButtonLabelButton.setTitle("NEXT", for: .normal)
+        }
+    }
+        
+        func isLightOn (_ color: UIView) -> Bool {
+            color.alpha == 1 ? true : false
+        }
+    }
+
 
