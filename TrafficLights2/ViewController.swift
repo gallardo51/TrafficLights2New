e5abbbeef06d4933d50b26ff5e88d8bbb310eac3
Alex Sobolev <31108@mail.ru>
2023-10-03 18:19:56 +0300

    Setup viewdidload

--- TrafficLights2/ViewController.swift
+++ TrafficLights2/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLightLabelView: UIView!
+    @IBOutlet weak var yellowLightLabelView: UIView!
+    @IBOutlet weak var greenLightLabelView: UIView!
+    
+    @IBOutlet weak var startButtonLabelButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightLabelView.alpha = 0.3
+        yellowLightLabelView.alpha = 0.3
+        greenLightLabelView.alpha = 0.3
+        
+        redLightLabelView.layer.cornerRadius = redLightLabelView.frame.width / 2
+        yellowLightLabelView.layer.cornerRadius = yellowLightLabelView.frame.width / 2
+        greenLightLabelView.layer.cornerRadius = greenLightLabelView.frame.width / 2
+        
+        startButtonLabelButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func startButtonPressed() {
+    }
+    
 }
 
